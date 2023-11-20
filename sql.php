afa81934aa67a7b0737c0eeef3c4fcac95c3e94d
EsaMayra <96944835+EsaMayra@users.noreply.github.com>
2023-11-20 10:04:44 +0800

    Add files via upload

---
+++ sql.php
@@ -0,0 +1,10 @@
+<?php
+
+    $server = "localhost";
+    $user = "root";
+    $password = "";
+    $database = "2101020029_esamayra";
+
+    $conn = mysqli_connect($server, $user, $password, $database);
+
+?>
